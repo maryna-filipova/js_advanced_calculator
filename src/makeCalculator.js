e675e57184a29c35e898ca2e69049a961a08e138
Maryna Filipova <doctor.who2016.dw@gmail.com>
2025-03-26 20:28:43 +0200

    solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,41 @@
  */
 function makeCalculator() {
   // write code here
+  return {
+    result: 0,
+
+    add(value) {
+      this.result += value;
+    },
+
+    subtract(value) {
+      this.result -= value;
+    },
+
+    multiply(value) {
+      this.result *= value;
+    },
+
+    /* divide(value) {
+      if (value !== 0) {
+        this.result /= value;
+      } else {
+        console.error(error);
+      }
+    }, */
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(operation, value) {
+      operation.call(this, value);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
